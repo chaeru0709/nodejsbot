b9a46daa95cc66663738741abcecc2fbe2ae8e01
chaeru0709 <kcw280709@gmail.com>
2020-07-06 21:23:50 +0900

    add bot state

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const byeChannelComment = "안녕히가세요.";
 
 client.on('ready', () => {
   console.log('켰다.');
-  client.user.setPresence({ game: { name: '채루서버 도움중.' }, status: 'online' })
+  client.user.setPresence({ game: { name: '명령어 하는중' }, status: 'online' })
 });
 
 client.on("guildMemberAdd", (member) => {
